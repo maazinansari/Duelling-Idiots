8c0ab8db209b0d6ee54ce025a8b88fdf99af116b
maazinansari <maazinansari@gmail.com>
2018-02-05 16:26:16 -0600

    completed 01-embarrasing-question.R

--- 01-embarrasing-question.R
+++ 01-embarrasing-question.R
@@ -11,4 +11,22 @@ set.seed(20180204)
 # P(heads second) = P(tails first) * P(heads second) = 0.25
 # P(tails second)
 # Find P(EQ = yes) = P(yes | heads first)
+# P(yes) = P(yes and heads first) + P(heads second) 
+#        = 0.5*p + 0.25 = 6230/10000
+# p = 0.746
 
+simulate_EQ = function(p = 0.746, N = 10000) {
+    pop = sample(c("yes", "no"), size = N, prob = c(p, 1-p), replace = TRUE)
+    results = rep("no", N)
+    for (i in 1:N) {
+        flip1 = rbinom(1, size = 1, p = 0.5) # 1 = heads, 0 = tails
+        if (flip1) results[i] = pop[i]
+        else {
+            flip2 = rbinom(1, size = 1, p = 0.5)
+            if (flip2) results[i] = "yes"
+        }
+    }
+    return(results)
+}
+
+sum(simulate_EQ() == "yes") # approx. 6230
